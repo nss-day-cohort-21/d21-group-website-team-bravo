b4fa07ccf34d00b749cb99d7cca879ab2ce0d266
Brooke Wittenberg <brooke.wittenberg@gmail.com>
2017-07-12 15:41:43 -0500

    update to index, js, add menu

---
+++ javascripts/main.js
@@ -0,0 +1,43 @@
+let menus = ['Brunch', 'Dinner', 'Drinks'];
+
+
+let menuItem1 = {}
+menuItem1.name = "Brunch" 
+// menuItem1.description = "Served from 10am to 3pm Saturday & Sunday";
+// menuItem1.price = "";
+// menuItem1.url = "";
+
+let menuItem2 = {}
+menuItem2.name = "Dinner"
+// menuItem2.description = "Served from 5pm to Close Tuesday through Sunday";
+// menuItem2.price = "";
+// menuItem2.url = "";
+
+let menuItem3 = {}
+menuItem3.name = "Cocktails"
+// menuItem3.description = "Craft cocktails served daily";
+// menuItem3.price = "";
+// menuItem3.url = "";
+
+console.log("menuItem1.name", menuItem1.name);
+
+
+
+
+
+
+
+
+
+
+
+// menu =document.getElementById("menu");
+
+
+
+// menuItem = `<h1 class="menu-item-title>${item.title}</h1>
+// 		<img src="${item.image}">
+// 		<p>${item.description}</p>`;
+		
+
+// menu.innerHTML += menuItem;
